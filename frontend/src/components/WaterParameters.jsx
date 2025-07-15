1289b85a2d48e90b9da39785b0ab76bb12928f38
Iyawn1210 <csoberano1210@gmail.com>
2025-07-15 15:36:46 +0800

    Chart implementation

--- frontend/src/components/WaterParameters.jsx
+++ frontend/src/components/WaterParameters.jsx
@@ -1,24 +1,127 @@
-import React from "react";
-// BUG: There is something wrong with the overall layout dashboard
+import React, { useState, useMemo, useEffect, useRef } from "react";
+import useChart from "./useChart.jsx";
+
 function WaterParameters({ sensorReadings, temp }) {
+  // Sample data for chart for hourly readings
+  const [chartType, setChartType] = useState("line");
+  const [chart, setChart] = useState("ammonia");
+
+  // create
+  const temperatureReading = useMemo(
+    () => ({
+      labels: ["7:00AM", "9:00AM", "11:00AM", "1:00PM", "3:00PM", "5:00PM"],
+      datasets: [
+        {
+          label: "Temperature (°C)",
+          data: [25, 25, 27, 28, 27, 27],
+          backgroundColor: "rgba(135, 206, 250, 0.4)",
+          borderColor: "rgba(135, 206, 250, 1)",
+          borderWidth: 2,
+          yAxisID: "y",
+        },
+      ],
+    }),
+    []
+  ); // Only recompute if the values change
+
+  const sensorData = useMemo(
+    () => ({
+      temperature: {
+        labels: ["7:00AM", "9:00AM", "11:00AM", "1:00PM", "3:00PM", "5:00PM"],
+        datasets: [
+          {
+            label: "Temperature (°C)",
+            data: [25, 25, 27, 28, 27, 27],
+            backgroundColor: "rgba(135, 206, 250, 0.4)",
+            borderColor: "rgba(135, 206, 250, 1)",
+            borderWidth: 2,
+            yAxisID: "y",
+          },
+        ],
+      },
+      pH: {
+        labels: ["7:00AM", "9:00AM", "11:00AM", "1:00PM", "3:00PM", "5:00PM"],
+        datasets: [
+          {
+            label: "pH Level",
+            data: [7.0, 7.2, 7.1, 8.2, 8.1, 8.5],
+            backgroundColor: "rgba(144, 238, 144, 0.4)",
+            borderColor: "rgba(144, 238, 144, 1)",
+            borderWidth: 2,
+            yAxisID: "y1",
+          },
+        ],
+      },
+      ammonia: {
+        labels: ["7:00AM", "9:00AM", "11:00AM", "1:00PM", "3:00PM", "5:00PM"],
+        datasets: [
+          {
+            label: "Ammonia (ppm)",
+            data: [0.2, 0.0, 0.5, 0.3, 0.1, 0.5],
+            backgroundColor: "rgba(255, 182, 193, 0.4)",
+            borderColor: "rgba(255, 182, 193, 1)",
+            borderWidth: 2,
+            yAxisID: "y2",
+          },
+        ],
+      },
+    }),
+    []
+  );
+
+  // const sensorOptions = useMemo(()=>({}),[])
+  // const options = useMemo(
+  //   () => ({
+  //     responsive: true,
+  //     scales: {
+  //       y: {
+  //         type: "linear",
+  //         position: "left",
+  //         title: { display: true, text: "Temperature (°C)" },
+  //       },
+  //       // y1: {
+  //       //   type: "linear",
+  //       //   position: "right",
+  //       //   grid: { drawOnChartArea: false },
+  //       //   title: { display: true, text: "pH" },
+  //       // },
+  //       // y2: {
+  //       //   type: "linear",
+  //       //   position: "right",
+  //       //   grid: { drawOnChartArea: false },
+  //       //   title: { display: true, text: "Ammonia (ppm)" },
+  //       //   offset: true,
+  //       // },
+  //     },
+  //   }),
+  //   []
+  // );
+
+  const chartRef = useChart(sensorData[chart], {}, chartType);
   return (
     <div className="water-parameter-data">
       <section className="sensor-data-section">
         <h1>Water Parameter Sensor Data</h1>
         <div className="sensor-data-container">
-          <div className="ammonia-container">
+          <div
+            className="ammonia-container"
+            onClick={() => setChart("ammonia")}
+          >
             <h2>Ammonia level</h2>
             <p className="ammonia-readings">{sensorReadings.ammonia} ppm</p>
           </div>
 
           {/* pH sensor readings */}
-          <div className="pH-container">
+          <div className="pH-container" onClick={() => setChart("pH")}>
             <h2>pH Level</h2>
             <p className="pH-readings">{sensorReadings.pH}</p>
           </div>
 
           {/* Temperature sensor readings */}
-          <div className="temperature-container">
+          <div
+            className="temperature-container"
+            onClick={() => setChart("temperature")}
+          >
             <h2>Temperature</h2>
             <p
               className="temperature-readings"
@@ -42,7 +145,9 @@ function WaterParameters({ sensorReadings, temp }) {
       </section>
       <section className="sensor-analytics-section">
         <h1>Sensor Analytics</h1>
-        <p>Analyze the trends and patterns in your sensor data over time.</p>
+        <div className="chart-container">
+          <canvas style={{ flex: "1", width: "100%" }} ref={chartRef} />
+        </div>
       </section>
     </div>
   );
