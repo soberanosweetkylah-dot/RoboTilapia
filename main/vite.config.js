073cff520729a73c6b0fb8fa299791539bfebffc
devyanx1210 <csoberano1210@gmail.com>
2025-09-13 19:55:38 +0800

    main

--- main/vite.config.js
+++ main/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 export default defineConfig({
   plugins: [react()],
   root: "./", // make sure this points to your current project root (main)
